c3344cb919492a5a61ddea694ee5f6e73c50e088
penta0410 <144640030+penta0410@users.noreply.github.com>
2024-04-21 02:43:22 +0900

    totyuu

--- Src/Scene/Play/ScenePlay.cpp
+++ Src/Scene/Play/ScenePlay.cpp
@@ -18,12 +18,14 @@ void PLAY::Init()
 	m_BG_x[0] = WINDOW_WIDTH/2;
 	m_BG_x[1] = WINDOW_WIDTH + (WINDOW_WIDTH / 2);
 	m_BG_y = WINDOW_HEIGHT/2;
+
 	//背景移動量
 	m_BG_move_x = 0;
 
+	//コイン初期化
+	InitCoin();
+	
 
-	
-	
 	//プレイループへ
 	g_CurrentSceneID = SCENE_ID_LOOP_PLAY;
 }
@@ -35,6 +37,7 @@ void PLAY::Load()
 	m_ImageHandle[1] = LoadGraph(PLAY_PATH[1]);			//プレイ背景2
 	m_ImageHandle[2] = LoadGraph(PLAY_PATH[2]);			//コイン
 	player.Load();				//プレイヤーの読み込み
+
 }
 
 //通常処理
@@ -59,12 +62,11 @@ void PLAY::Step()
 	//マップの当たり判定
 	MapCollision(-m_BG_move_x);
 
+	//コイン当たり判定
+	CoinCollision(-m_BG_move_x);
+
 	//プレイヤーの座標を更新
 	player.UpdatePos();
-
-	
-
-	
 
 	//リザルトシーンへの遷移
 	//Enterキー押されたなら
@@ -85,17 +87,12 @@ void PLAY::Draw()
 	DrawRotaGraph(m_BG_x[0], m_BG_y, 1.0f, 0.0f, m_ImageHandle[0], true);
 	DrawRotaGraph(m_BG_x[1], m_BG_y, 1.0f, 0.0f, m_ImageHandle[1], true);
 
-	DrawRotaGraph(2000 + m_BG_move_x, 400, 1.0f, 0.0f, m_ImageHandle[2], true);
-	DrawRotaGraph(1950 + m_BG_move_x, 450, 1.0f, 0.0f, m_ImageHandle[2], true);
-	DrawRotaGraph(2050 + m_BG_move_x, 450, 1.0f, 0.0f, m_ImageHandle[2], true);
-	DrawRotaGraph(1900 + m_BG_move_x, 500, 1.0f, 0.0f, m_ImageHandle[2], true);
-	DrawRotaGraph(2100 + m_BG_move_x, 500, 1.0f, 0.0f, m_ImageHandle[2], true);
-
 	m_map.Draw(-m_BG_move_x);	//マップ描画
 
 	player.Draw();				//プレイヤーの描画処理
 
-	
+	//コイン
+	DrawCoin(m_BG_move_x);
 
 	//デバッグ
 	SetFontSize(30);
@@ -160,7 +157,6 @@ void PLAY::MapCollision(int mapmove)
 					player.PlayerCeiling();
 
 				}
-
 				//下方向の修正
 				if (dirArray[1]) {
 					// めり込み量を計算する
@@ -213,7 +209,6 @@ void PLAY::MapCollision(int mapmove)
 					int overlap = Bx + Bw - Ax;
 					player.SetNextPosX(Ax + overlap);
 				}
-
 				// 右方向の修正
 				if (dirArray[3]) {
 					// めり込み量を計算する
@@ -223,109 +218,46 @@ void PLAY::MapCollision(int mapmove)
 			}
 		}
 	}
+}
 
-	////敵
-	////Y方向のみ当たり判定をチェックする
-	//for (int mapIndexY = 0; mapIndexY < MAP_DATA_Y; mapIndexY++)
-	//{
-	//	for (int mapIndexX = 0; mapIndexX < MAP_DATA_X; mapIndexX++)
-	//	{
-	//		//ブロック以外は処理しない
-	//		if (m_Map.m_FileReadMapData[mapIndexY][mapIndexX] == MAPCHIP_NONE)
-	//			continue;
+//コイン座標初期化
+void PLAY::InitCoin()
+{
+	for (int coin_num; coin_num < COIN_NUM; coin_num++)
+	{
+		m_coin_x[coin_num] = COIN_PLACE[coin_num][0];
+		m_coin_y[coin_num] = COIN_PLACE[coin_num][1];
+		CoinFlag[coin_num] = 1;
+	}
+}
 
+//コイン当たり判定
+void PLAY::CoinCollision(int mapmove)
+{
+	for (int coin_num = 0; coin_num < COIN_NUM; coin_num++)
+	{
+		//コインが描画されていたら
+		if (CoinFlag[coin_num] == 1)
+		{
+			////当たっているかチェック
+			//if (IsHitRect()) 
+			//{
+			//	//コイン描画フラグoff
+			//	CoinFlag[coin_num] = 0;
 
-	//		//どの方向に進んでいたかチェック
-	//		bool dirArray[4] = { false,false,false,false };
-	//		m_enemy.GetMoveDirection(dirArray);
+			//}
+		}
+	}
+}
 
-
-	//		//矩形の当たり判定用のデータを準備
-	//		//敵の情報
-	//		int Ax = m_enemy.GetPosX() - 30;
-	//		int Ay = m_enemy.GetPosY() - 30;
-	//		int Aw = ENEMY_W + 30;
-	//		int Ah = ENEMY_H + 30;
-
-	//		//オブジェクトの情報
-	//		int Bx = mapIndexX * MAP_SIZE - 240;
-	//		int By = mapIndexY * MAP_SIZE + 30;
-	//		int Bw = MAP_SIZE - 30;
-	//		int Bh = MAP_SIZE;
-
-	//		//Y方向のみに移動したと仮定した座標で当たり判定をチェックします
-	//		Ay = m_enemy.GetNextPosY();
-	//		Ax = m_enemy.GetPosX();
-
-	//		//当たっているかチェック
-	//		if (IsHitRect(Ax, Ay, Aw, Ah, Bx, By, Bw, Bh))
-	//		{
-
-	//			// 上方向の修正
-	//			if (dirArray[0]) {
-	//				// めり込み量を計算する
-	//				int overlap = By + Bh - Ay;
-	//				m_enemy.SetPosY(Ay + overlap);
-	//			}
-
-	//			//下方向の修正
-	//			if (dirArray[1]) {
-	//				// めり込み量を計算する
-	//				int overlap = Ay + Ah - By;
-	//				m_enemy.SetPosY(Ay - overlap);
-	//			}
-	//		}
-	//	}
-	//}
-
-	//// X方向のみ当たり判定をチェックする
-	//for (int mapIndexY = 0; mapIndexY < MAP_DATA_Y; mapIndexY++)
-	//{
-	//	for (int mapIndexX = 0; mapIndexX < MAP_DATA_X; mapIndexX++)
-	//	{
-	//		// ブロック以外は処理しない
-	//		if (m_Map.m_FileReadMapData[mapIndexY][mapIndexX] == MAPCHIP_NONE)
-	//			continue;
-
-	//		//どの方向に進んでいたかチェック
-	//		bool dirArray[4] = { false,false,false,false };
-	//		m_enemy.GetMoveDirection(dirArray);
-
-	//		//敵の情報
-	//		int Ax = m_enemy.GetPosX() - 30;
-	//		int Ay = m_enemy.GetPosY() - 30;
-	//		int Aw = ENEMY_W;
-	//		int Ah = ENEMY_H;
-
-	//		//オブジェクトの情報
-	//		int Bx = mapIndexX * MAP_SIZE - 240;
-	//		int By = mapIndexY * MAP_SIZE + 30;
-	//		int Bw = MAP_SIZE - 30;
-	//		int Bh = MAP_SIZE;
-
-	//		//X方向のみに移動したと仮定した座標で当たり判定をチェック
-	//		Ay = m_enemy.GetPosY();
-	//		Ax = m_enemy.GetPosX();
-
-	//		// 当たっているかチェック
-	//		if (IsHitRect(Ax, Ay, Aw, Ah, Bx, By, Bw, Bh))
-	//		{
-
-	//			// 左方向の修正
-	//			if (dirArray[2]) {
-	//				// めり込み量を計算する
-	//				int overlap = Bx + Bw - Ax;
-	//				m_enemy.SetPosX(Ax + overlap);
-	//			}
-
-	//			// 右方向の修正
-	//			if (dirArray[3]) {
-	//				// めり込み量を計算する
-	//				int overlap = Ax + Aw - Bx;
-	//				m_enemy.SetPosX(Ax - overlap);
-	//			}
-	//		}
-	//	}
-	//}
-
+//コイン描画
+void PLAY::DrawCoin(int mapmove)
+{
+	for (int coin_num; coin_num < COIN_NUM; coin_num++)
+	{
+		if (CoinFlag[coin_num] == 1)
+		{
+			DrawRotaGraph(m_coin_x[coin_num] + mapmove, m_coin_y[coin_num], 1.0f, 0.0f, m_ImageHandle[2], true);
+		}
+	}
 }
--- Src/Scene/Play/ScenePlay.h
+++ Src/Scene/Play/ScenePlay.h
@@ -12,6 +12,9 @@
 //プレイシーン画像枚数
 const int PLAY_IMAGE_PATH_NUM = 3;
 
+//コイン枚数
+const int COIN_NUM = 200;
+
 //プレイシーンパス
 const char PLAY_PATH[PLAY_IMAGE_PATH_NUM][256] =
 {
@@ -21,6 +24,14 @@ const char PLAY_PATH[PLAY_IMAGE_PATH_NUM][256] =
 };
 
 const char PLAY_SCENE_BGM[] = { "" };				//プレイシーンのBGM
+
+//コイン座標
+//第一ｘ座標、第二ｙ座標
+const int COIN_PLACE[COIN_NUM][2] =
+{
+	{2000, 400},{1950, 450},{2050, 450},{1900, 500},{2100, 500},
+
+};
 
 //プレイシーンクラス
 class PLAY : public SCENE_BASE
@@ -41,6 +52,12 @@ private:
 	//背景移動量
 	int m_BG_move_x;
 
+	//コイン座標
+	int m_coin_x[COIN_NUM];
+	int m_coin_y[COIN_NUM];
+	//コイン描画フラグ
+	int CoinFlag[COIN_NUM];
+
 public:
 
 	//初期化
@@ -55,6 +72,12 @@ public:
 	void Fin() override;
 	// マップの当たり判定
 	void MapCollision(int mapmove);
+	//コイン座標初期化
+	void InitCoin();
+	//コイン描画
+	void DrawCoin(int mapmove);
+	//コイン当たり判定
+	void CoinCollision(int mapmove);
 
 };
 
